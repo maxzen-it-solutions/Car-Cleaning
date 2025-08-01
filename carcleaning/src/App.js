d5bc7d8ef0d958d33498372dc9f0779291fd4d08
pratap495 <etakarlapallipratap@gmail.com>
2025-08-01 11:24:06 +0530

    file updated

--- carcleaning/src/App.js
+++ carcleaning/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+
 
 function App() {
   return (
     <div className="App">
-      <p className='text-8xl'>i am a good boy</p>
+      <Navbar/>
+
     </div>
   );
 }
