27e9861b17b4ed7e3dafd94e9baeb8924803b51f
pratap495 <etakarlapallipratap@gmail.com>
2025-08-01 11:06:32 +0530

    created a file

---
+++ carcleaning/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='text-8xl'>i am a good boy</p>
+    </div>
+  );
+}
+
+export default App;
