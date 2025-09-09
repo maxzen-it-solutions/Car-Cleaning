f9ec465c83f5c290d044ecd947a27d348046389e
pratap495 <etakarlapallipratap@gmail.com>
2025-09-10 02:16:28 +0530

    finishs

--- Frontend/src/services/apiService.js
+++ Frontend/src/services/apiService.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiService = createApi({
     reducerPath: 'apiService',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'https://carsbuddy.net/api',
+        baseUrl: 'http://localhost:5000/api',
         prepareHeaders: (headers) => {
             const token = localStorage.getItem('token');
             if (token) {
